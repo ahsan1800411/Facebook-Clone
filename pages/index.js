d847353ad078ee48137b44f92bb1ccd4d8c0d917
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-10-04 11:49:01 +0500

    Sidebar component

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Sidebar from "../components/Sidebar";
 export default function Home({ session }) {
   if (!session) return <Login />;
   return (
-    <div>
+    <div className='h-screen bg-gray-100 overflow-hidden'>
       <Head>
         <title>Facebook</title>
       </Head>
