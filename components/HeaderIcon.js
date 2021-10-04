68a5c6c6446379b30391502af8dde4bd15b1beae
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-10-04 09:56:39 +0500

    Header component

---
+++ components/HeaderIcon.js
@@ -0,0 +1,11 @@
+export default function HeaderIcon({ active, Icon }) {
+  return (
+    <div className='cursor-pointer  md:px-10 sm:h-14 md:hover:bg-gray-100 flex items-center rounded-xl active:border-b-2 active:border-blue-500 group'>
+      <Icon
+        className={`h-5 group-hover:text-blue-500 text-center sm:h-7 mx-auto text-gray-500 ${
+          active && "text-blue-500"
+        }`}
+      />
+    </div>
+  );
+}
